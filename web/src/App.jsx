6fe265096874baf09c6c21ce81733ac312878d04
jagoanonline932-hue <jagoanonline932@gmail.com>
2026-05-20 02:37:23 +0700

    Create App.jsx

---
+++ web/src/App.jsx
@@ -0,0 +1,20 @@
+export default function App() {
+  return (
+    <div
+      style={{
+        minHeight: "100vh",
+        display: "flex",
+        alignItems: "center",
+        justifyContent: "center",
+        fontFamily: "Arial",
+        background: "#0f172a",
+        color: "white",
+        flexDirection: "column",
+      }}
+    >
+      <h1>HyperDrop SaaS</h1>
+      <p>Deploy Vercel berhasil 🚀</p>
+    </div>
+  );
+}
+
