833a35397e92d71dda7c4df72d0f82624189cc31
jagoanonline932-hue <jagoanonline932@gmail.com>
2026-05-21 01:45:14 +0700

    Refactor imports in main.jsx for consistency

--- web/src/main.jsx
+++ web/src/main.jsx
@@ -1,10 +1,8 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
-import {
-  RouterProvider,
-} from "react-router-dom";
+import { RouterProvider } from "react-router-dom";
 
-import router from "./routes";
+import router from "./routes.jsx";
 
 ReactDOM.createRoot(document.getElementById("root")).render(
   <React.StrictMode>
